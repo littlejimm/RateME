661e8cddcca44a262400fc618702a12f19b667bc
littlejimm <seojumper@gmail.com>
2011-02-08 03:00:51 +0100

    Fourth

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
+<html>
+<header> <title>Testing Page</title></header>
+<body>
+
+
 <?php
 
+$int = 10;
+
+if($int<5)
+{
 echo "Testing Git commit";
+}
+else
+{
+echo "Testing Git commit 2";
+}
+
 
 ?>
+
+</body>
+</html>
